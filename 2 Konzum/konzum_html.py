264d4d5e006d892a27abd8070b6e41095c31e0be
Zoran Kradija <zoran.kradija@hotmail.com>
2023-03-25 21:11:40 +0100

    Verzija 1.0

---
+++ 2 Konzum/konzum_html.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+import xlsxwriter
+import math
+import datetime
+import time
+
+# zadatak: sa internet stranice https://Posta.hr skinuti sva mjesta, poštanske urede i županije
