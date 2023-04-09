3417a5712705d25f599e64b5d6e8e28982dfd01e
Zoran Kradija <zoran.kradija@hotmail.com>
2023-04-09 21:24:09 +0200

    Verzija 1.0

--- 5 Rabljeni auti/main.py
+++ 5 Rabljeni auti/main.py
@@ -1,6 +1,7 @@
-import index, neostar, dasweltauto
+import index, neostar, dasweltauto, trcz
 
 if __name__ == '__main__':
     #index.oglasi()
     #neostar.oglasi()
     dasweltauto.oglasi()
+    #trcz.oglasi()
