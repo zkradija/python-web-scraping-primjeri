dc9cfb33792d8d9241475d6ff7b7c5bed8b197bb
Zoran Kradija <zoran.kradija@hotmail.com>
2023-04-11 20:07:07 +0200

    Verzija 1.0

--- 5 Rabljeni auti/main.py
+++ 5 Rabljeni auti/main.py
@@ -10,15 +10,16 @@ def ocisti():
 # definiranje argumenata za poziv iz komandne linije
 def args():
     argParser = argparse.ArgumentParser()
-    argParser.add_argument('-w', '--workers', type=int, default=24, help='broj radnika u poolu')
-    argParser.add_argument('-t', '--time_sleep', type=float, default=0.10, help='pauza između dohvata podataka')
+    argParser.add_argument('-w', '--workers', type=int, default=6, help='broj radnika u poolu')
+    argParser.add_argument('-t', '--time_sleep', type=float, default=1, help='pauza između dohvata podataka')
     args = argParser.parse_args()
     return args.workers, args.time_sleep
 
 if __name__ == '__main__':
     workers, time_sleep = args()
-    # index.oglasi(workers, time_sleep)
-    # ocisti()
+    ocisti()
+    index.oglasi(workers, time_sleep)
+    ocisti()
     neostar.oglasi(workers, time_sleep)
     ocisti()
     dasweltauto.oglasi(workers, time_sleep)
