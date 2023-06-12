ada6e31497e782ea23862cb27f97d5bf53fadee5
Zoran Kradija <zoran.kradija@hotmail.com>
2023-06-12 18:43:47 +0200

    dorada ad 1)

--- 1 Pošta/posta_html.py
+++ 1 Pošta/posta_html.py
@@ -2,13 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import xlsxwriter
 import math
-import datetime
+from datetime import datetime
 import time
 
 
 # zadatak: sa intrnet stranice https://Posta.hr skinuti sva mjesta, poštanske urede i županije
-# usporediti ćemo 2 načina parsiranja: lxml i html.parse
-# html --> prosječno vrijeme 120 sekundi
+# prosječno vrijeme 120 sekundi
 
 #identificiram se kao Firefox browser
 headers = {
@@ -16,39 +15,39 @@ headers = {
     "Accept-Encoding": "*",
     "Connection": "keep-alive"}
 
-url_baza='https://www.posta.hr/pretrazivanje-mjesta-s-pripadajucim-postanskim-brojem/1403?pojam=&page='
+url_base='https://www.posta.hr/pretrazivanje-mjesta-s-pripadajucim-postanskim-brojem/1403?pojam=&page='
+
 data=[]
 
-pocetak_vrijeme = time.time()
+start_time = time.perf_counter()
 s = requests.Session()
 
-
-#prvo tražim broj rezultata, koji dijelim s 20 (rezultata po stranici) --> tako dobijem zadnju stranicu
-response = s.get(url_baza + '1', headers=headers)
+# prvo tražim broj rezultata, koji dijelim s 20 (rezultata po stranici) --> tako dobijem zadnju stranicu
+response = s.get(f'{url_base}1', headers=headers)
 web_page = response.content
 soup = BeautifulSoup(web_page, 'html.parser')
+
 last_page_text = soup.find('div', attrs={'class':'results-found'}).get_text()
-last_page_list = [int(x) for x in last_page_text.split() if x.isdigit()]    #tražim samo brojeve, unutar teksta
-last_page = math.ceil((int(last_page_list[0])/20))  #dijelim sa 20 rezultata po stranici, te zaokružujem na broj više
-print('Zadnja stranica pronađena: ' + str(last_page) + ' --> ' + datetime.now().strftime('%H:%M:%S') + ' h')
+last_page_list = [int(x) for x in last_page_text.split() if x.isdigit()]    # tražim samo brojeve, unutar teksta
+last_page = math.ceil((int(last_page_list[0])/20))  # dijelim sa 20 rezultata po stranici, te zaokružujem na broj više
 
-for i in range (1,last_page+1):
+print(f'Zadnja stranica pronađena: {last_page} --> {datetime.now().strftime("%H:%M:%S")} h')
 
-    response = s.get(url_baza + str(i), headers=headers)
+for i in range (1, last_page+1):
+    response = s.get(f'{url_base}i', headers=headers)
     web_page = response.content
     soup = BeautifulSoup(web_page, 'html.parser')
     table = soup.find('table', attrs={'class':'tablica-borders'})
     table_body = table.find('tbody')
-
     rows = table_body.find_all('tr')
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele]) # Get rid of empty values
-    if i != 0 and (i == 1 or i % 50 == 0): print('Stranica broj: ' + str(i) + ' / ' + str(last_page) + ' --> ' + datetime.now().strftime('%H:%M:%S') + ' h')
+        data.append([ele for ele in cols if ele]) # rješavam se praznih redova
+    if i != 0 and (i == 1 or i % 50 == 0): print(f'Stranica broj: {i} / {last_page} --> {datetime.now().strftime("%H:%M:%S")} h')
 
 
-#rješavam se praznih
+# rješavam se praznih polja
 data2 = [x for x in data if x]
 
 with xlsxwriter.Workbook('Poštanski_brojevi_html.xlsx') as workbook:
@@ -57,7 +56,7 @@ with xlsxwriter.Workbook('Poštanski_brojevi_html.xlsx') as workbook:
     worksheet.write(0, 1, 'postanski_ured')
     worksheet.write(0, 2, 'zupanija')
     for row_num, data2 in enumerate(data2):
-        worksheet.write_row(row_num+1, 0, data2)
-kraj_vrijeme = time.time()
-ukupno_vrijeme=kraj_vrijeme-pocetak_vrijeme
-print(ukupno_vrijeme)
+        worksheet.write_row(row_num + 1, 0, data2)
+end_time = time.perf_counter()
+elapsed_time = int(end_time) - int(start_time)
+print(elapsed_time)
